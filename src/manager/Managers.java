94357192871aa5d1320eaf3f16bf8cd6ae118876
Dimout00 <dimout00@yandex.ru>
2025-09-07 20:44:30 +0500

    сохранение истории в файл

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,8 +1,14 @@
 package manager;
 
+import java.io.File;
+
 public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager(getDefaultHistory());
+    }
+
+    public static TaskManager getFileBackedManager(File file) {
+        return new FileBackedTaskManager(file);
     }
 
     public static HistoryManager getDefaultHistory() {
