f966d9267fd9e75c1e6aa1826178d15b16972d5d
Dimout00 <dimout00@yandex.ru>
2025-09-16 21:46:33 +0500

    code style3

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Epic extends Task {
     private List<Integer> subtaskIds;
